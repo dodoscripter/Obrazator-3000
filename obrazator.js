71ff7e0164598e5763f189f29bbea64b8a1d739e
IgorOczk <lolwatt221@gmail.com>
2016-12-31 22:33:06 +0100

    Update obrazator.js

--- obrazator.js
+++ obrazator.js
@@ -1,6 +1,6 @@
 var losoweCzesciCiala = ["Twarz", "Noga", "Ręka", "Miednica"];
 var losowePrzymiotniki = ["Cuchnąca", "Brudna", "Głupia", "Brzydka", "Okropna"];
-var losoweSlowa = ["Mucha", "Glista", "Małpa", "Oliwia", "Kawa", "Odbytnica"]
+var losoweSlowa = ["Mucha", "Glista", "Małpa", "Jaszczurka", "Kawa", "Odbytnica"]
 var losowaCzescCiala = losoweCzesciCiala[Math.floor(Math.random() * 4)];
 var losowyPrzymiotnik = losowePrzymiotniki[Math.floor(Math.random() * 5)];
 var losoweSlowo = losoweSlowa[Math.floor(Math.random() * 6)];
