df6e4323cecedcd1811ceb20ec349f8ea49a03e1
IgorOczk <lolwatt221@gmail.com>
2016-12-31 22:15:11 +0100

    Update obrazator.js

--- obrazator.js
+++ obrazator.js
@@ -5,4 +5,4 @@ var losowaCzescCiala = losoweCzesciCiala[Math.floor(Math.random() * 4)];
 var losowyPrzymiotnik = losowePrzymiotniki[Math.floor(Math.Random() * 5)];
 var losoweSlowo = losoweSlowa[Math.floor(Math.Random() * 6)];
 var zdanie = "Twoja " + "losowaCzescCiala" + "jest jak " + "losowyPrzymiotnik" + "losoweSlowo"
-zdanie
+alert(zdanie);
